a4e8952b96187acdaf0ac0e1dd1506cbbbc4421c
Diyaharish <86552912+Diyaharish@users.noreply.github.com>
2022-01-23 15:32:06 +0530

    odd.java
    Program to check whether a given number is odd or even.

---
+++ Cycle1/odd.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class odd
+{
+    public static void main(String args[])
+    {
+     Scanner obj=new Scanner(System.in);
+     int n;
+     System.out.println("Enter the number");
+     n=obj.nextInt();
+     if(n%2==0)
+     System.out.println(n+" is an even number");
+     else 
+     System.out.println(n+" is an odd number");
+    }
+}
