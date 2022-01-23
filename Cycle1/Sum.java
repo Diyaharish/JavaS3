3e51dc4c9a7c5c877fa5ff13a504e51f58613fd9
Diyaharish <86552912+Diyaharish@users.noreply.github.com>
2022-01-23 15:30:06 +0530

    Sum.java
    Program to add two numbers.

---
+++ Cycle1/Sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Sum 
+{
+    public static void main(String args[])
+    {
+    Scanner obj=new Scanner(System.in);
+    int a,b,sum;
+    System.out.println("Enter first number");
+    a=obj.nextInt();
+    System.out.println("Enter second number");
+    b=obj.nextInt();
+    sum=a+b;
+    System.out.println("Sum = "+sum);
+  }
+}
