9315311f2c4c44b9e639814bf37c5eaa7daedb6f
Diyaharish <86552912+Diyaharish@users.noreply.github.com>
2022-01-25 00:42:38 +0530

    program7.java
    Java program to create a class employee with details such as empno, name and phone number. Read details of at least 5 employees and print.

---
+++ Cycle3/program7.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class employee 
+{
+    String name ;
+    int empno,pn;
+    void EnterDetails()  
+      {
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter Name :");
+        name = obj.nextLine();  
+       
+        System.out.print("Enter Empo No :"); 
+        empno = obj.nextInt();
+        
+        System.out.print("Enter Phone no :"); 
+        pn = obj.nextInt();
+      }
+       
+    void PrintDetails()
+      {
+        
+        System.out.println("Name : "+name);
+        
+        System.out.println("Empo No : "+empno); 
+
+        System.out.println("Phone no : "+pn); 
+      }
+
+    public static void main(String args[]) 
+      {
+        int i;  
+        employee obj[] = new employee[50];
+
+        for(i=0;i<5;i++) 
+          {  
+            obj[i] = new employee();
+            obj[i].EnterDetails(); 
+          }
+          System.out.println("Details");
+          for(i=0;i<5;i++) 
+          {
+            obj[i].PrintDetails();
+          }
+       }
+}
+
+
