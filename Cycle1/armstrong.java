c89b4f27aaf0b4e503dd2f1d99316925bbd681ec
Diyaharish <86552912+Diyaharish@users.noreply.github.com>
2022-01-26 01:30:00 +0530

    armstrong.java
    Program to check whether the given number is Armstrong or not.

--- Cycle1/armstrong.java
+++ Cycle1/armstrong.java
@@ -1,23 +1,33 @@
 import java.util.*;
-class armstrong
+class armstrong 
 {
-    public static void main(String args[])
+    public static void main(String  args[])
     {
-    Scanner obj=new Scanner(System.in);
-    int n,n1,d,s=0;
-    System.out.println("Enter a number");
-    n=obj.nextInt();
-    n1=n;
-    while(n!=0)
-    {
-        d=n%10;
-        s=(d*d*d)+s;
-        n/=10;
+        Scanner obj=new Scanner(System.in);
+        int count=0,n,d,D=0,n1,i;
+        System.out.println("Enter a number");
+        n=obj.nextInt();
+        n1=n;
+        while(n>0)
+        {
+            n=n/10;
+            count=count+1;
+        }
+        n=n1;
+        while(n>0)
+        {
+            int multi=1;
+            d=n%10;
+            for(i=0;i<count;i++)
+            {
+                multi=multi*d;
+            }
+            n=n/10;
+            D=D+multi;
+        }
+        if(n1==D)
+        System.out.println("This is an Armstrong number");
+        else
+        System.out.println("This is not an Armstrong number");
     }
-    if(n1==s)
-    System.out.println(n1+" is an armstrong number");
-    else 
-    System.out.println(n1+" is not an armstrong number");
-
-}
 }
