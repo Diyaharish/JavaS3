893d89fc703ea7de289e1c021c4ccf538cacd218
Diyaharish <86552912+Diyaharish@users.noreply.github.com>
2022-02-15 21:45:39 +0530

    Create balanceacc.java

---
+++ Cycle6/Q1/balanceacc.java
@@ -0,0 +1,22 @@
+lass balanceacc
+{
+	public static void main(String args[])
+	{
+		try{
+			int b=100;
+			if(b<500)
+				throw new balanceException();
+		}
+		catch(balanceException e)
+		{
+			System.out.println(e);
+		}
+	}
+}
+class balanceException extends Exception
+{
+	balanceException()
+	{
+		System.out.println("Low balance");
+	}
+}
