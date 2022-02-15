bf7bc4a97a9b43f6285c50ff0060d4e112be40b3
Diyaharish <86552912+Diyaharish@users.noreply.github.com>
2022-02-15 21:41:27 +0530

    Create arithmexp.java

---
+++ Cycle6/Q1/arithmexp.java
@@ -0,0 +1,15 @@
+class arithmexp
+{
+	public static void main(String args[])
+	{
+		try{
+			int x,y=5;
+			x=y/0;
+			System.out.println(b);
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println(e);
+		}
+	}
+}
